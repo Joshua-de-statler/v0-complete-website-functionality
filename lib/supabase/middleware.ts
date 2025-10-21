f26b8fa8d85455b732bb169705dab769c1ec3191
Jeandre Coetzee <Jeandre@Alvirnos-MacBook-Air.local>
2025-10-21 21:15:58 +0200

    force redeploy

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -14,7 +14,8 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        
+        // random comments
+        // another comment
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
